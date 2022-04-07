66416b51cd65075e5b17a8e0faec1ad5433a8676
Binh Bui <binhbv@gmail.com>
2022-04-07 15:18:18 +0700

    publish

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import crypto from 'crypto';
-import request from 'requestretry';
-import LRU from 'lru-cache';
+import crypto from "crypto";
+import request from "requestretry";
+import LRU from "lru-cache";
 
 // must start with https:// and we only want to download certificates from AWS
-const CERT_URL_PATTERN = /^https:\/\/sns\.[a-zA-Z0-9-]{3,}\.amazonaws\.com(\.cn)?\/SimpleNotificationService-[a-zA-Z0-9]{32}\.pem$/;
+const CERT_URL_PATTERN =
+  /^https:\/\/sns\.[a-zA-Z0-9-]{3,}\.amazonaws\.com(\.cn)?\/SimpleNotificationService-[a-zA-Z0-9]{32}\.pem$/;
 
 // maximum of 5000 certificates and the certificates expire after 1 minute from the cache.
 const CERT_CACHE = new LRU({ max: 5000, ttl: 1000 * 60 });
@@ -26,7 +27,7 @@ const fetchCert = (certUrl: string) => {
 
     request(
       {
-        method: 'GET',
+        method: "GET",
         url: certUrl,
         maxAttempts: 3,
         retryDelay: 100,
@@ -59,69 +60,68 @@ const fetchCert = (certUrl: string) => {
  */
 const fieldsForSignature = (type: string) => {
   if (
-    type === 'SubscriptionConfirmation' ||
-    type === 'UnsubscribeConfirmation'
+    type === "SubscriptionConfirmation" ||
+    type === "UnsubscribeConfirmation"
   ) {
     return [
-      'Message',
-      'MessageId',
-      'SubscribeURL',
-      'Timestamp',
-      'Token',
-      'TopicArn',
-      'Type',
+      "Message",
+      "MessageId",
+      "SubscribeURL",
+      "Timestamp",
+      "Token",
+      "TopicArn",
+      "Type",
     ];
-  } else if (type === 'Notification') {
-    return ['Message', 'MessageId', 'Subject', 'Timestamp', 'TopicArn', 'Type'];
+  } else if (type === "Notification") {
+    return ["Message", "MessageId", "Subject", "Timestamp", "TopicArn", "Type"];
   } else {
     return [];
   }
 };
 
 export class MessageValidator {
-    /**
-     * Verifies the signature of the message.
-     * @param message sns incoming message
-     * @returns true if the signature is valid
-     */
+  /**
+   * Verifies the signature of the message.
+   * @param message sns incoming message
+   * @returns true if the signature is valid
+   */
   async validate(message: any): Promise<any> {
-
     // we do some input validation:
     // 1. the fields SignatureVersion, SigningCertURL, Type, and Signature must be available
     if (
       !(
-        'SignatureVersion' in message &&
-        'SigningCertURL' in message &&
-        'Type' in message &&
-        'Signature' in message
+        "SignatureVersion" in message &&
+        "SigningCertURL" in message &&
+        "Type" in message &&
+        "Signature" in message
       )
     ) {
-      throw new Error('missing field');
+      throw new Error("missing field");
     }
 
     // 2. SignatureVersion must be 1
-    if (message.SignatureVersion !== '1') {
-      throw new Error('invalid SignatureVersion');
+    if (message.SignatureVersion !== "1") {
+      throw new Error("invalid SignatureVersion");
     }
 
     // 3. The SigningCertURL must start with https:// and we only want to download certificates from AWS
     if (!CERT_URL_PATTERN.test(message.SigningCertURL)) {
-      throw new Error('invalid certificate URL');
+      throw new Error("invalid certificate URL");
     }
 
     // 4. Validate the signature by downloading the certificate and verifying the signature
     const certificate: any = await fetchCert(message.SigningCertURL);
-    const verify = crypto.createVerify('sha1WithRSAEncryption');
+    const verify = crypto.createVerify("sha1WithRSAEncryption");
     fieldsForSignature(message.Type).forEach((key) => {
       if (key in message) {
         verify.write(`${key}\n${message[key]}\n`);
       }
     });
     verify.end();
-    const result = verify.verify(certificate, message.Signature, 'base64');
+    const result = verify.verify(certificate, message.Signature, "base64");
 
     if (!result) {
-      throw new Error('The message signature is invalid.');
+      throw new Error("The message signature is invalid.");
     }
     return message;
   }
